86117eccd9f10635265341baa4abf74142f8d33f
Anton <115146037+Kollqq@users.noreply.github.com>
2024-05-28 10:45:55 +0200

    Add files via upload

---
+++ lab1/klocki.pl
@@ -0,0 +1,13 @@
+% ===================================
+
+% na (X, Y)
+% opis: spelniony, gdy klocek X lezy bezposrednio na klocku Y
+% -----------------------------na/2
+ na(c,a).
+ na(c,b).
+ na(d,c).
+% -----------------------------na/2
+% 3 klauzule
+% 1 definicja relacji
+% relacja na/2, ktora sklada sie z:
+% 3 klauzuli, ktore sa faktami
